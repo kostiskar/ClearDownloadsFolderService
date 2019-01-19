e5fd20c9e35eec5b5314000df68e9fb632eff701
deigre <deigre@DESKTOP-PTMIS6V>
2019-01-19 11:52:02 +0200

    Upload Project Files

---
+++ ClearDownloadsFolderService/ClearDownloadsFolder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ClearDownloadsFolderService
+{
+    public class ClearDownloadsFolder
+    {
+        private readonly Timer _timer;
+
+        // Get the path from the config file
+        string folderPath = ConfigurationManager.AppSettings["FolderPath"].ToString();
+
+        public ClearDownloadsFolder()
+        {
+            // The Service runs in a loop every 5 days
+            _timer = new Timer(432000*1000) { AutoReset = true };
+
+            // Create the event
+            _timer.Elapsed += HandleTimerElapsed;
+        }
+
+        // Specify what you want to happen when the Elapsed event is  
+        // raised.
+        private void HandleTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            DirectoryInfo folderWithFilesToBeDeleted = new DirectoryInfo(folderPath);
+
+            // Delete all files at that directory
+            foreach(FileInfo file in folderWithFilesToBeDeleted.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+
+        // Start() should be implemented in a service
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        // Stop() should be implemented in a service
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ ClearDownloadsFolderService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ClearDownloadsFolderService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start the service, run until it stops and then grab exitCode
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<ClearDownloadsFolder>(s =>
+                {
+                    s.ConstructUsing(clearDownLoadsFolder => new ClearDownloadsFolder());
+                    s.WhenStarted(clearDownLoadsFolder => clearDownLoadsFolder.Start());
+                    s.WhenStopped(clearDownLoadsFolder => clearDownLoadsFolder.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                // Configure the 'machine-friendly' name
+                x.SetServiceName("ClearDownloadsFolderService");
+
+                // Configure the description
+                x.SetDisplayName("This is a service that clears the Downloads folder" +
+                                 "every 1 minute");
+            });
+
+            // Convert from enum to integer and passing it out
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
